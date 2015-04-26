c2ba3a9806a853d4622f3424ce71e5cfd658ccea
Tyler Youschak <tyleryouschak@gmail.com>
2015-04-25 22:56:22 -0400

    Updating Lumen to v5.0.4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,12 +10,14 @@ Dotenv::load(__DIR__.'/../');
 |--------------------------------------------------------------------------
 |
 | Here we will load the environment and create the application instance
-| that servers as the central piece of the framework. We'll use this
+| that serves as the central piece of this framework. We'll use this
 | application as an "IoC" container and router for this framework.
 |
 */
 
-$app = new Laravel\Lumen\Application;
+$app = new Laravel\Lumen\Application(
+	realpath(__DIR__.'/../')
+);
 
 // $app->withFacades();
 
