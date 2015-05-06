891d02491dffdec9f5b47989b73e3a2351b79ce8
Tyler Youschak <tyleryouschak@gmail.com>
2015-05-06 08:29:07 -0400

    Forgot to copy data to above

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -52,14 +52,17 @@ class HomeController extends Controller {
 	    $disk_used = intval($disk_result[2]);
 	    $disk = intval(rtrim($disk_result[4], "%"));
 
-	    $memory = 0;
-	    if($this->mem_used == "0" || $this->mem_total == "0") {
+	    if($this->mem_used !== "0" || $this->mem_total !== "0") {
 	    	$memory = round($this->mem_used / $this->mem_total * 100);
+		} else {
+			$memory = 0;
 		}
 
-		$swap = 0;
-		if($this->swap_used == "0" || $this->swap_total == "0") {
+		
+		if($this->swap_used !== "0" || $this->swap_total !== "0") {
 	    	$swap = round($this->swap_used / $this->swap_total * 100);
+		} else {
+			$swap = 0;
 		}
 
 
